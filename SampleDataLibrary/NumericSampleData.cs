756163cad74406e97f91b42b9418b63c3898c0ed
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-10 18:51:57 +0200

    Numeric Sample Data
    created methods that output: sample bool, int(with min and max), double( with min and max), phone number and zip code with support of numeric stule formats provided when method beeing called

--- SampleDataLibrary/NumericSampleData.cs
+++ SampleDataLibrary/NumericSampleData.cs
@@ -6,5 +6,51 @@ using System.Threading.Tasks;
 
 namespace SampleDataLibrary
 {
-    
+    public class NumericSampleData
+    {
+        private Random rand = new();
+        public bool sampleBool()
+        {
+            if (rand.Next() % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int sampleInt(int minRange, int maxRange)
+        {
+            return rand.Next(minRange, maxRange);
+        }
+        public double sampleDouble(double minRange, double maxRange)
+        {
+            return Math.Round((rand.NextDouble() * (maxRange - minRange) + minRange), 3);
+        }
+        public string samplePhoneNumber(string format)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sb.Append(rand.Next(1, 9));
+            }
+
+            return String.Format(format, double.Parse(sb.ToString()));
+
+        }
+
+        public string sampleZipCode(string format)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 9; i++)
+            {
+                sb.Append(rand.Next(1, 9));
+            }
+
+            return String.Format(format, double.Parse(sb.ToString()));
+        }
+    }
 }
