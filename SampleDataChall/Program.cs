3764c60e0f1f7ebfaf596c2d8de19a10bb0a5adf
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-10 18:52:36 +0200

    UI sample code
    created code in console UI that shows sample data creation logic

--- SampleDataChall/Program.cs
+++ SampleDataChall/Program.cs
@@ -1,1 +1,41 @@
 ﻿
+using SampleDataLibrary;
+
+NamesSampleData namesSample = new();
+NumericSampleData numericSample = new();
+
+Console.WriteLine("Names:");
+for (int i = 1; i < 31; i++)
+{
+    Console.WriteLine($"{i}. {namesSample.SampleFirstName()} {namesSample.SampleLastName()} ");
+}
+
+Console.WriteLine("\n\n\n\nBools:");
+for (int i = 1; i < 31; i++)
+{
+    Console.WriteLine($"{i}. {numericSample.sampleBool()} ");
+}
+
+Console.WriteLine("\n\n\n\nInts from 0 to 99:");
+for (int i = 1; i < 31; i++)
+{
+    Console.WriteLine($"{i}. {numericSample.sampleInt(0, 99)} ");
+}
+
+Console.WriteLine("\n\n\n\nDoubles from 1 to 19:");
+for (int i = 1; i < 31; i++)
+{
+    Console.WriteLine($"{i}. {numericSample.sampleDouble(1, 19)} ");
+}
+
+Console.WriteLine("\n\n\n\nPhone numbers:");
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine(numericSample.samplePhoneNumber("{0:(###) ###-####}"));
+}
+
+Console.WriteLine("\n\n\n\nZip Codes:");
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine(numericSample.sampleZipCode("{0:#####-####}"));
+}
