bb00bb54cfe69b94674a8de2928a9e4d9d85cc4b
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-10 18:50:33 +0200

    NamesSampleData
    Created class with methods to output sample name and surname from txt file filled with names

--- SampleDataLibrary/NamesSampleData.cs
+++ SampleDataLibrary/NamesSampleData.cs
@@ -6,5 +6,21 @@ using System.Threading.Tasks;
 
 namespace SampleDataLibrary
 {
-    
+    public class NamesSampleData
+    {
+        private Random rand = new();
+        public string SampleFirstName()
+        {
+            List<string> firstNames = File.ReadAllLines("firstnames.txt").ToList();
+            int sample = rand.Next(0, firstNames.Count);
+            return firstNames[sample];
+
+        }
+        public string SampleLastName()
+        {
+            List<string> lastNames = File.ReadAllLines("lastnames.txt").ToList();
+            int sample = rand.Next(0, lastNames.Count);
+            return lastNames[sample];
+        }
+    }
 }
